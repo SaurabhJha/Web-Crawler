1b0f1a72b742d188eea164dc3f8a854464466dd5
Saurabh Jha <saurabh.jhaa@gmail.com>
2013-10-20 02:40:33 +0530

    Cleaning up

--- process.py
+++ process.py
@@ -11,26 +11,20 @@ class Process:
      seen -> contains a list of urls that have been downloaded. A python list
      fronier -> contains a list of urls that have been discovered but are yet
      to be downloaded.
-     level -> level in a breadth first traveresal graph
      harvest -> Web pages that are downloaded.
-     
+
      """
      def __init__(self, url):
           self.url = url
           self.seen = []
           self.frontier = deque()
-          self.level = 0
           self.harvest = []
 
-     def run(self, url = None, level = None):
+     def run(self, url = None):
           """
           Performs the actual crawling. Comes with an optional argument
           If an explicit url is specified, the run takes that url as
           seed.
-          It also comes with a optional level argument. If specified, the
-          crawling will be done only upto that level of the breadth first
-          search tree.
-          # level functionality not implemented
           """
           if url == None:
                url = self.url
@@ -45,7 +39,5 @@ class Process:
           for l in links:
                parse_result = urlparse(l)
                self.frontier.append(l)
-          length_frontier = len(self.frontier)
-          if 
           for l in self.frontier:
                self.run(l)

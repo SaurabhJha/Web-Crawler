17905e4894bb5f887e8fb0b74ab244733aa047b5
Saurabh Jha <saurabh.jhaa@gmail.com>
2013-10-20 17:39:08 +0530

    Removed the url attribute of Process class. The run now takes a single url argument and downloads from there. Simplifies the design due to rid of excessive variables

--- process.py
+++ process.py
@@ -5,42 +5,42 @@ import urllib2
 
 class Process:
      """
-     This is a crawler process, also known as worker thread. It takes
-     a seed url and  crawls from there. The basic attributes of Process are
+     This is a crawler process, also known as worker thread. It simulates the
+     process of taking a seed url and crawling from there. The basic attributes
+     of Process are
 
-     url -> seed url
      seen -> contains a list of urls that have been downloaded. A python list
      fronier -> contains a list of urls that have been discovered but are yet
      to be downloaded.
      harvest -> Web pages that are downloaded.
 
      """
-     def __init__(self, url):
-          self.url = url
+     def __init__(self):
           self.seen = []
           self.frontier = deque()
           self.harvest = []
 
-     def run(self, custom_url = None):
+     def run(self, url):
           """
-          Performs the actual crawling. Comes with an optional argument
-          If an explicit url is specified, the run takes that url as
-          seed.
+          Performs the actual crawling. Takes a seed url as an argument and
+          performs a downloading of that url and extracts links on that webpage.
+          Then it downlaods the webpages addressed by the links on the previous
+          webpage.
+
+          In summary, it performs a breadth first search from the seed url.
+
           """
-          if custom_url == None:
-               current_url = self.url
-          else:
-               current_url = custom_url
-          b = Bot(current_url)
-          try :
+          b = Bot(url)
+          try:
                downloaded_url = b.crawl()
           except AttributeError:
-               current_url = self.frontier.popleft()
+               url = self.frontier.popleft()
+               b = Bot(url)
                downloaded_url = b.crawl()
           except urllib2.URLError:
-               current_url = self.frontier.popleft()
-               downloaded_url = b.crawl()               
-          downloaded_url = b.crawl()
+               url = self.frontier.popleft()
+               b = Bot(url)
+               downloaded_url = b.crawl()
           print downloaded_url
           self.seen.append(downloaded_url)
           self.harvest.append(downloaded_url)
